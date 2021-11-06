2e4521f93a86d4ad4775a745fa51ff74f2b26e29
EsotericZ <cjsand03@gmail.com>
2021-11-06 10:00:32 -0600

    js code worked on adding and hiding buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,40 @@
 
 function startQuiz() {
     console.log("You hit the button")
+    // alert("You hit the button");
+    document.getElementById("tagb").hidden = true;
+    quiz();
 }
 
-// const $ul = document.createElement('ul');
-// const $button = document.getElementById('hobbies-generator');
-// document.body.appendChild($ul);
-// $button.addEventListener('click', function() {
-//   const numOfHobbies = prompt('How many hobbies do you want?');
-//   for (let i = 0; i < parseInt(numOfHobbies); i++) {
-//     const userHobby = prompt("Tell me a hobby you would like to add")
-//     const newLi = document.createElement('li');
-//     newLi.textContent = userHobby;
-//     $ul.appendChild(newLi);
-//   }
-// })
-
 let tag = document.createElement("button");
-tag.type = "submit";
-tag.name = "sbtn";
+tag.innerHTML = "Click";
+// tag.type = "submit";
+// tag.name = "sbtn";
+tag.id = "tagb";
 tag.addEventListener("click", startQuiz);
 document.body.appendChild(tag);
+
+let question = document.createElement("h1");
+let ans1 = document.createElement("button");
+let ans2 = document.createElement("button");
+let ans3 = document.createElement("button");
+let ans4 = document.createElement("button");
+
+let qa = [
+    ["q1", "a1", "a2", "a3", "a4"],
+    ["q2", "a1", "a2", "a3", "a4"]
+ ];
+
+function quiz(){
+    question.textContent = qa[0][0];
+    ans1.innerHTML = qa[0][1];
+    ans2.innerHTML = qa[0][2];
+    ans3.innerHTML = qa[0][3];
+    ans4.innerHTML = qa[0][4];
+    document.body.appendChild(question);
+    document.body.appendChild(ans1);
+    document.body.appendChild(ans2);
+    document.body.appendChild(ans3);
+    document.body.appendChild(ans4);
+    // ans1.innerHTML = qa[0][1];
+}
