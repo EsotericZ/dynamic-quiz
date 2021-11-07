d2b51d79c73fd4b34b8e413e581486e3a553f7e7
EsotericZ <cjsand03@gmail.com>
2021-11-06 21:09:42 -0600

    created the js to move from question 1 to n with a click

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,21 +23,67 @@ let ans2 = document.createElement("button");
 let ans3 = document.createElement("button");
 let ans4 = document.createElement("button");
 
-let qa = [
-    ["q1", "a1", "a2", "a3", "a4"],
-    ["q2", "a1", "a2", "a3", "a4"]
- ];
+// let qa = [
+//     ["q1", ["a1", 0], ["a2", 1], ["a3", 0], ["a4", 0]],
+//     ["q2", ["a1", 0], ["a2", 0], ["a3", 1], ["a4", 0]]
+//  ];
 
-function quiz(){
-    question.textContent = qa[0][0];
-    ans1.innerHTML = qa[0][1];
-    ans2.innerHTML = qa[0][2];
-    ans3.innerHTML = qa[0][3];
-    ans4.innerHTML = qa[0][4];
+// function quiz(){
+//     question.textContent = qa[0][0];
+//     ans1.innerHTML = qa[0][1][0];
+//     ans2.innerHTML = qa[0][2][0];
+//     ans3.innerHTML = qa[0][3][0];
+//     ans4.innerHTML = qa[0][4][0];
+//     document.body.appendChild(question);
+//     document.body.appendChild(ans1);
+//     document.body.appendChild(ans2);
+//     document.body.appendChild(ans3);
+//     document.body.appendChild(ans4);
+// }
+
+let questions = [
+    {question: "This is the first question",
+    answer: [{ans: "answer 1-01", tf: true}, 
+        {ans: "answer 1-02", tf: false}, 
+        {ans: "answer 1-03", tf: false}, 
+        {ans: "answer 1-04", tf: false}]},
+    {question: "This is the second question",
+    answer: [{ans: "answer 2-01", tf: true}, 
+        {ans: "answer 2-02", tf: false}, 
+        {ans: "answer 2-03", tf: false}, 
+        {ans: "answer 2-04", tf: false}]},
+    {question: "This is the third question",
+    answer: [{ans: "answer 3-01", tf: true}, 
+        {ans: "answer 3-02", tf: false}, 
+        {ans: "answer 3-03", tf: false}, 
+        {ans: "answer 3-04", tf: false}]}
+]
+
+let current = 0;
+function quiz() {
+    question.textContent = questions[current].question;
+    ans1.innerHTML = questions[current].answer[0].ans;
+    ans2.innerHTML = questions[current].answer[0].ans;
+    ans3.innerHTML = questions[current].answer[2].ans;
+    ans4.innerHTML = questions[current].answer[3].ans;
+
     document.body.appendChild(question);
     document.body.appendChild(ans1);
     document.body.appendChild(ans2);
     document.body.appendChild(ans3);
     document.body.appendChild(ans4);
-    // ans1.innerHTML = qa[0][1];
+
+    ans1.addEventListener("click", next);
+    // allbtn = document.getElementById('button')
+    // console.log(allbtn)
+    // document.addEventListener("click", quiz())
 }
+
+function next(){
+    console.log("next!")
+    current++
+    console.log(current)
+    quiz()
+}
+
+
