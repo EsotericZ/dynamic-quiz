8e5900ddba975e1c76b924ee157cc93769beebf7
EsotericZ <cjsand03@gmail.com>
2021-11-06 22:18:54 -0600

    js added input for name and high score area with restart button that infinite loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 function startQuiz() {
-    console.log("You hit the button")
     document.getElementById("tagb").hidden = true;
     quiz();
 }
 
 let tag = document.createElement("button");
-tag.innerHTML = "Click";
+tag.innerHTML = "Start!";
 tag.id = "tagb";
 tag.addEventListener("click", startQuiz);
 document.body.appendChild(tag);
@@ -15,6 +14,17 @@ let ans1 = document.createElement("button");
 let ans2 = document.createElement("button");
 let ans3 = document.createElement("button");
 let ans4 = document.createElement("button");
+
+let hsname = document.createElement("input");
+let hsbtn = document.createElement("button");
+hsname.type = "text";
+hsname.id = "hsname";
+hsbtn.id = "hsbtn";
+
+let rstart = document.createElement("button");
+rstart.id = "rstart";
+
+
 
 let questions = [
     {question: "This is the first question",
@@ -32,10 +42,23 @@ let questions = [
         {ans: "answer 3-02", tf: false}, 
         {ans: "answer 3-03", tf: false}, 
         {ans: "answer 3-04", tf: false}]}
-]
+];
 
 let current = 0;
+let redo = 0;
+
+
+
+
+
 function quiz() {
+    if (redo !== 0) {
+        document.getElementById("rstart").hidden = true;
+        document.getElementById("ans1b").hidden = false;
+        document.getElementById("ans2b").hidden = false;
+        document.getElementById("ans3b").hidden = false;
+        document.getElementById("ans4b").hidden = false;   
+    }
     question.textContent = questions[current].question;
     ans1.innerHTML = questions[current].answer[0].ans;
     ans2.innerHTML = questions[current].answer[0].ans;
@@ -66,11 +89,38 @@ function next(){
 }
 
 function scores() {
-    document.getElementById("ques").hidden = true;
+    if (redo !== 0) {
+        document.getElementById("hsname").hidden = false;
+        document.getElementById("hsbtn").hidden = false;   
+    }
     document.getElementById("ans1b").hidden = true;
     document.getElementById("ans2b").hidden = true;
     document.getElementById("ans3b").hidden = true;
     document.getElementById("ans4b").hidden = true;    
+
+    ques.innerHTML = "Congrats! You Finished the Quiz!"
+    document.body.appendChild(hsname);
+    document.body.appendChild(hsbtn);
+    hsbtn.innerHTML = "Submit";
+
+    hsbtn.addEventListener("click", scoreboard);
 }
 
+function scoreboard() {
+    if (redo !== 0) {
+        document.getElementById("rstart").hidden = false;
+    }
+    document.getElementById("hsname").hidden = true;
+    document.getElementById("hsbtn").hidden = true;
+    
+    ques.innerHTML = "High Scores"
+    document.body.appendChild(rstart);
+    rstart.innerHTML = "Try Again"
+    rstart.addEventListener("click", restart)
+}
 
+function restart() {
+    current = 0;
+    redo++
+    quiz()
+}
